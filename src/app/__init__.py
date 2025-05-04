5eb6a58e7f421849ba226b7933b3bdf1b2e1a256
Charu Bhadauria <charubhadauria@Charus-MacBook-Air.local>
2025-05-04 21:31:55 +0530

    Initial commit

---
+++ src/app/__init__.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask import request,jsonify
+from app.service import messageService
+from service.messageService import MessageService
+from kafka import KafkaProducer
+import json
+
+app = Flask(__name__)
+app.config.from_pyfile('config.py')
+
+messageService = MessageService()
+
+producer = KafkaProducer(bootstrap_servers = ['localhost:9092'],
+                         value_serializer = lambda v: json.dumps(v).encode('utf-8')
+                         )
+
+@app.route('/v1/ds/message',methods = ['POST'])
+def handle_message():
+    message = request.json.get('message')
+    result = messageService.process_message(message)
+    serialized_result = result.json()
+
+    producer.send('your_kafka_topic',serialized_result)
+    return jsonify(result)
+
+@app.route('/',methods=['GET'])
+def hangle_get():
+    print("Hello World")
+
+if __name__ == "__main__":
+    app.run(host="localhost",port="8000",debug=True)
